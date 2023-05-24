ab9e99f84682c13fc0227eae2f06068dfffd0d95
Snazo09 <119118651+Snazo09@users.noreply.github.com>
2023-05-24 23:56:10 +0200

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
-"""Module for User class"""
+"""Defines the User class."""
 from models.base_model import BaseModel
 
 
 class User(BaseModel):
-    """User Class"""
+    """Represent a User.
 
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
+    Attributes:
+        email (str): The email of the user.
+        password (str): The password of the user.
+        first_name (str): The first name of the user.
+        last_name (str): The last name of the user.
+    """
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
